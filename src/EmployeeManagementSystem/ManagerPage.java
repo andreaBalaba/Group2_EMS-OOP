39ff5d84368268539bfae3b9913c15126b66bbf5
andengcute <andreabalaba0827@gmail.com>
2023-07-06 20:24:27 +0800

    update code

--- src/EmployeeManagementSystem/ManagerPage.java
+++ src/EmployeeManagementSystem/ManagerPage.java
@@ -19,13 +19,20 @@ public class ManagerPage extends JFrame implements ActionListener
         setLayout(null);
 
         titleLabel = new JLabel("Employee Management System");
-        titleLabel.setFont(new Font("Open Sans", Font.BOLD, 18));
-        titleLabel.setBounds(235, 225, 400, 35);
+        titleLabel.setFont(new Font("Open Sans", Font.BOLD, 25));
+        titleLabel.setBounds(180, 70, 400, 35);
+        
+        ImageIcon icon2 = new ImageIcon(ClassLoader.getSystemResource("EmployeeManagementSystem/Icon/ems.png"));
+        Image Icon2 = icon2.getImage().getScaledInstance(550,350,Image.SCALE_DEFAULT);
+        ImageIcon pic2 = new ImageIcon(Icon2);
+        JLabel pics2 = new JLabel(pic2);
+        pics2.setBounds(100, 100, 550, 350);
         
         menuBar.setBackground(Color.WHITE);
         
+        
         menu1 = new JMenu("Profile");
-        menu1.setFont(new Font("Open Sans", Font.PLAIN, 13));
+        menu1.setFont(new Font("Open Sans", Font.PLAIN, 14));
         JMenuItem menuItem1 = new JMenuItem("Add New Employee Profile");
         JMenuItem menuItem2 = new JMenuItem("View Profile");
         menuItem1.setFont(new Font("Open Sans", Font.PLAIN, 12));
@@ -34,7 +41,7 @@ public class ManagerPage extends JFrame implements ActionListener
         menuItem2.addActionListener(this);
        
         menu2 = new JMenu("Manage");
-        menu2.setFont(new Font("Open Sans", Font.PLAIN, 13));
+        menu2.setFont(new Font("Open Sans", Font.PLAIN, 14));
         JMenuItem menuItem3 = new JMenuItem("Update Employee Details");
         JMenuItem menuItem4 = new JMenuItem("Delete Employee");
         menuItem3.setFont(new Font("Open Sans", Font.PLAIN, 12));
@@ -43,7 +50,7 @@ public class ManagerPage extends JFrame implements ActionListener
         menuItem4.addActionListener(this);
         
         menu3 = new JMenu("Request");
-        menu3.setFont(new Font("Open Sans", Font.PLAIN, 13));
+        menu3.setFont(new Font("Open Sans", Font.PLAIN, 14));
         JMenuItem menuItem5 = new JMenuItem("Time Off");
         JMenuItem menuItem6 = new JMenuItem("Expenses");
         menuItem5.setFont(new Font("Open Sans", Font.PLAIN, 12));
@@ -52,13 +59,13 @@ public class ManagerPage extends JFrame implements ActionListener
         menuItem6.addActionListener(this);
         
         menu4 = new JMenu("Review");
-        menu4.setFont(new Font("Open Sans", Font.PLAIN, 13));
+        menu4.setFont(new Font("Open Sans", Font.PLAIN, 14));
         JMenuItem menuItem7 = new JMenuItem("Performance Review");
         menuItem7.setFont(new Font("Open Sans", Font.PLAIN, 12));
         menuItem7.addActionListener(this);
         
         menu5 = new JMenu("Exit");
-        menu5.setFont(new Font("Open Sans", Font.PLAIN, 13));
+        menu5.setFont(new Font("Open Sans", Font.PLAIN, 14));
         JMenuItem menuItem8 = new JMenuItem("Log Out");
         menuItem8.setFont(new Font("Open Sans", Font.PLAIN, 12));
         menuItem8.addActionListener(this);
@@ -71,15 +78,17 @@ public class ManagerPage extends JFrame implements ActionListener
         menu3.add(menuItem5);
         menu3.add(menuItem6);
         menu4.add(menuItem7);
-        menu4.add(menuItem8);
+        menu5.add(menuItem8);
         
         menuBar.add(menu1);
         menuBar.add(menu2);
         menuBar.add(menu3);
         menuBar.add(menu4);
         menuBar.add(menu5);
+        
 
         add(titleLabel);
+        add(pics2);
         setJMenuBar(menuBar);
 
         setVisible(true);
@@ -105,14 +114,6 @@ public class ManagerPage extends JFrame implements ActionListener
         {
             new DeleteEmployee();
         }
-        else if (comnd.equals("Take Attendance"))
-        {
-            
-        }
-        else if (comnd.equals("View Attendance"))
-        {
-        
-        }
         else if (comnd.equals("Time Off"))
         {
         
@@ -127,9 +128,16 @@ public class ManagerPage extends JFrame implements ActionListener
         }
         else if (comnd.equals("Log Out"))
         {
-            dispose(); 
-            new WelcomePage();
+            dispose();
+            SwingUtilities.invokeLater(new Runnable() 
+            {
+            @Override
+                 public void run() 
+                 {
+                     new WelcomePage();
+                }
+            });
         }
      
- }
+    }
 }
